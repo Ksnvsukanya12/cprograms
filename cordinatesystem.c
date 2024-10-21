0336ed09f9a50ade7d3ee7d00f72d2acee19d533
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-21 11:36:15 +0530

    Update cordinatesystem.c

--- cordinatesystem.c
+++ cordinatesystem.c
@@ -1,20 +1,27 @@
-// Online C compiler to run C program online
+/// Online C compiler to run C program online
 #include <stdio.h>
 #include <math.h>
 void main() {
- int a,b,c;   // Write C code here
+ int a,b,c,d;   // Write C code here
  printf("enter a,b,c");
  float r1,r2;
-scanf("%d%d%d",&a&b&c);
+scanf("%d%d%d",&a,&b,&c);
 d=(b*b)-(4*a*c);
 if(d>0)
 {
 printf("roots are real and unequal");
-r1=-b=sqrt(d)/2*a;
+r1=-b-sqrt(d)/2*a;
 r2=-b-sqrt(d)/2*a;
+ printf("r1=%f,r2=%f",r1,r2);
 }
-else
+else if(d<0)
 {
-    printf("odd");
+    printf("roots are imaginary");
 }
-}    
+ else
+{
+printf("roots  real and equal");
+r1=r2=-b/2*a;
+printf("r1=%f,r2=%f",r1,r2);
+}
+}
