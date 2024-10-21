64597747f50c24fb748e9e4516ce4edfadc86200
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-21 14:41:07 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,15 @@
+/// Online C compiler to run C program online
+#include <stdio.h>
+void main() {
+char ch;   // Write C code here
+ printf("enter ch");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+    printf("ch is an vowel");
+}
+else
+{
+    printf("ch is not an vowel");
+}
+}
