38a0b287d680224ab3466b035816a7e0934e8bb2
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-21 12:34:25 +0530

    Create largestusingterinary.c

---
+++ largestusingterinary.c
@@ -0,0 +1,8 @@
+/// Online C compiler to run C program online
+#include <stdio.h>
+void main() {
+int a,b,c,large;   // Write C code here
+ printf("enter a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+large=((a>b&&a>c)?a:(b>c?b:c));
+}
