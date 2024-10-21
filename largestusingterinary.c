e0ea5a25d935a1b02a811cec2802e5f820568219
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-21 14:25:05 +0530

    Update largestusingterinary.c

--- largestusingterinary.c
+++ largestusingterinary.c
@@ -4,5 +4,6 @@ void main() {
 int a,b,c,large;   // Write C code here
  printf("enter a,b,c");
 scanf("%d%d%d",&a,&b,&c);
-large=((a>b&&a>c)?a:(b>c?b:c));
+large = ((a>b&&a>c)?a:(b>c?b:c));
+printf("the largest one is:%d",large);
 }
