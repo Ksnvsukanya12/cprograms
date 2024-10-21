ff5902f3d2e5258d3a6a0219249f29a47157013a
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-21 12:03:42 +0530

    Create eligibleforvote.c

---
+++ eligibleforvote.c
@@ -0,0 +1,15 @@
+/// Online C compiler to run C program online
+#include <stdio.h>
+void main() {
+ int age ;   // Write C code here
+ printf("enter age");
+scanf("%d",&age);
+if(age>=18)
+{
+    printf("eligible for vote");
+}
+else
+{
+    printf("not eligible for vote");
+}
+}
