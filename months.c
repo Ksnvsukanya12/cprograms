1fa89f45858e85cfcf6faddae43600132b954b5a
Ksnvsukanya12 <sukanyakagitha95@gmail.com>
2024-10-28 12:31:02 +0530

    Create months.c

---
+++ months.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void main() {
+int a ;  
+printf("enter number");
+scanf("%d",&a);
+switch(a)
+{
+    case 1 : printf("jan");
+             break;
+    case 2 : printf("feb");
+             break;
+    case 3 :printf("march");
+             break;
+    case 4 : printf("april");
+             break;
+    case 5 : printf("may");
+             break;
+    case 6 : printf("june");
+             break;
+    case 7 : printf("july");
+             break;
+    case 8 : printf("august");
+             break;
+    case 9 : printf("sept");
+             break;
+    case 10 : printf("oct");
+             break;
+    case 11 : printf("nov");
+             break;
+    case 12 : printf("dec");
+             break;
+    default : printf("in valid output");
+              break;
+}
+}
